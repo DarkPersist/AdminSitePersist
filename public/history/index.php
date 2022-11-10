e9ef09034c42c5753ad7d32e51d7d8210dd0d6ab
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2022-11-10 12:54:21 -0500

    Update index.php

--- public/history/index.php
+++ public/history/index.php
@@ -3,45 +3,56 @@ include '../../server/connection/conexion.php';
 include '../../server/security/seguridad.php';
 ?>
 <?php if (!empty($user)) : ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <?php include "../resources/assets/meta.html" ?>
-</head>
+    <head>
+        <?php include "../resources/assets/meta.html" ?>
+    </head>
 
-<body>
-    <?php include "../resources/partials/header--default.html" ?>
-    <div class="container-fluid text-light" style="background-color: #0D0D0D;">
-        <div class="grid text-center">
-            <div class="mb-5">
-                <h1>Acciones</h1>
-            </div>
-            <div>
-                <div class="row">
-                    
+    <body>
+        <?php include "../resources/partials/header--main.html" ?>
+        <div class="container-fluid text-light" style="background-color: #0D0D0D;">
+            <div class="grid text-center">
+                <div class="mb-5">
+                    <h1>Acciones</h1>
+                </div>
+                <div>
+                    <div class="row">
+                        <?php foreach (mysqli_query($conx, "SELECT * FROM acciones") as $data) : ?>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?php echo $data['action']?></h5>
+                                        <p class="card-text"><?php echo $data['description']?>.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <?php include "../resources/partials/footer--default.html" ?>
-</html>
+        <?php include "../resources/partials/footer--default.html" ?>
+
+    </html>
 <?php else : ?>
 
-<!DOCTYPE html>
-<!--html sino ha iniciado sesion-->
-<html>
+    <!DOCTYPE html>
+    <!--html sino ha iniciado sesion-->
+    <html>
 
-<body>
-    <script>
-        setTimeout(alertFunc, 1000);
+    <body>
+        <script>
+            setTimeout(alertFunc, 1000);
 
-        function alertFunc() {
-            location.replace("../login");
-        }
-    </script>
-</body>
+            function alertFunc() {
+                location.replace("../login");
+            }
+        </script>
+    </body>
 
-</html>
+    </html>
 
 <?php endif; ?>

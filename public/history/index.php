26d10e2f1f5aedd7d5035c6b05d5659e58a059aa
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2022-11-10 12:09:27 -0500

    main

---
+++ public/history/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "../../resources/assets/meta.html" ?>
+</head>
+
+<body>
+    <?php include "../../resources/partials/header--default.html" ?>
+    <div class="container-fluid text-light" style="background-color: #0D0D0D;">
+        <div class="grid text-center">
+            <div class="mb-5">
+                <h1>Equipos</h1>
+            </div>
+            <div>
+                <div class="row">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "../../resources/partials/footer--default.html" ?>
+</html>
